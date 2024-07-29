174c6f31863ea0c88167fa669460d4ce6784eafc
om salunke <salunkeom474@gmail.com>
2024-07-29 03:57:53 -0700

    login form design completed

---
+++ app/component/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className='w-full py-4  flex flex-col justify-center items-center'>
+        <img src='/merilogo.png' className='w-20 my-4'/>
+        <h1 className='text-xl'>Welcome to Meri Panchayat</h1>
+    </div>
+  )
+}
+
+export default Banner
---
+++ app/login/page.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Banner from "../component/Banner";
+import Link from "next/link";
+
+const Page = () => {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center w-full h-auto">
+        <Banner />
+        <form className="max-w-sm mx-auto box-shadow w-[auto] h-[auto] px-10 py-16">
+          <h1 className="w-full text-center h-8 font-semibold text-xl mb-6">
+            Login to your account
+          </h1>
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+            >
+              Your email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-200 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="name@gmail.com"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-black"
+            >
+              Your password
+            </label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter password"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-200 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <div className="flex items-start mb-6">
+            <div className="flex items-center h-6 w-full">
+              <Link href={"/signup"} className=" w-full text-right text-purple-500 underline-offset-2 underline">Create a account</Link>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Page;
